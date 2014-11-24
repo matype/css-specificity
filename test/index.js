3e294a235914488cb473ce6c8661790694f2b9f3
Masaaki Morishita <ikasama3.0@gmail.com>
2014-11-24 18:42:03 +0900

    add test code

--- test/index.js
+++ test/index.js
@@ -1,6 +1,24 @@
 var test = require('colored-tape')
-var css-specificity = require('..')
+var specificity = require('..')
 
-test('description', function (t) {
-  t.end()
+var selector = '.class, p.sel:after'
+
+test('specificity.calc()', function (t) {
+    t.same(specificity.calc(selector), [ [ 0, 0, 1, 0 ], [ 0, 0, 1, 2 ] ])
+    t.end()
+})
+
+test('specificity.a()', function (t) {
+    t.same(specificity.a(selector), [ 0, 0 ])
+    t.end()
+})
+
+test('specificity.b()', function (t) {
+    t.same(specificity.b(selector), [ 1, 1 ])
+    t.end()
+})
+
+test('specificity.c()', function (t) {
+    t.same(specificity.c(selector), [ 0, 2 ])
+    t.end()
 })
