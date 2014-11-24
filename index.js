7c5ec053ec928838db1daafd1316c6def6b002a6
Masaaki Morishita <ikasama3.0@gmail.com>
2014-11-25 01:03:16 +0900

    change calc() response and add vs() method

--- index.js
+++ index.js
@@ -1,29 +1,48 @@
 var specimen = require('specimen')
 
 module.exports.calc = function (selector) {
-    return specimen(selector)
+    return calc(selector)
 }
 
-module.exports.a = function (selector) {
-    return calcIndex(selector, 1)
-}
-
-module.exports.b = function (selector) {
-    return calcIndex(selector, 2)
-}
-
-module.exports.c = function (selector) {
-    return calcIndex(selector, 3)
-}
-
-function calcIndex (selector, index) {
-    var calcResults = specimen(selector)
-    if (Array.isArray(calcResults[0])) {
-        var resArr = []
-        calcResults.forEach(function (result) {
-            resArr.push(result[index])
-        })
-        return resArr
+module.exports.vs = function (selector1, selector2) {
+    var res1 = calc(selector1)[0]
+    var res2 = calc(selector2)[0]
+    for (var i = 1; i < 4; i++) {
+        if (res1.specificity[i] != res2.specificity[i]) {
+            var winner = Math.max(res1.specificity[i], res2.specificity[i])
+            if (winner == res1.specificity[i]) return selector1
+            else if( winner == res2.specificity[i]) return selector2
+        }
     }
-    return calcResults[index]
+    return 'draw'
+}
+
+function calc (selector) {
+    var selectors = arraySelector(selector)
+    var res = []
+    selectors.forEach(function (selector) {
+        var calcResult = specimen(selector)
+        res.push({
+            'selector': selector,
+            'specificity': calcResult,
+            'a': calcResult[1],
+            'b': calcResult[2],
+            'c': calcResult[3]
+        })
+    })
+    return res
+}
+
+function arraySelector (selector) {
+    var arrSel = selector.split(',')
+    arrSel.forEach(function (sel, i) {
+        arrSel[i] = sel.trim()
+    })
+    return arrSel
+}
+
+function isOnlyClassSelector (selector) {
+    console.log(selector)
+    if (selector.match(/^\.|\s/)) return true
+    return false
 }
--- test/index.js
+++ test/index.js
@@ -1,24 +1,43 @@
 var test = require('colored-tape')
 var specificity = require('..')
 
-var selector = '.class, p.sel:after'
+var selector = '.class, p.sel:after, #id:before'
+var selector2 = 'div'
 
 test('specificity.calc()', function (t) {
-    t.same(specificity.calc(selector), [ [ 0, 0, 1, 0 ], [ 0, 0, 1, 2 ] ])
+    var expected = [
+        {
+            'selector': '.class',
+            'specificity': [0, 0, 1, 0],
+            'a': 0,
+            'b': 1,
+            'c': 0
+        },
+        {
+            'selector': 'p.sel:after',
+            'specificity': [0, 0, 1, 2],
+            'a': 0,
+            'b': 1,
+            'c': 2
+        },
+        {
+            'selector': '#id:before',
+            'specificity': [0, 1, 0, 1],
+            'a': 1,
+            'b': 0,
+            'c': 1
+        },
+    ]
+    t.same(specificity.calc(selector), expected)
     t.end()
 })
 
-test('specificity.a()', function (t) {
-    t.same(specificity.a(selector), [ 0, 0 ])
+test('specificity.vs()', function (t) {
+    t.same(specificity.vs('ul.class', 'class'), 'ul.class')
     t.end()
 })
 
-test('specificity.b()', function (t) {
-    t.same(specificity.b(selector), [ 1, 1 ])
-    t.end()
-})
-
-test('specificity.c()', function (t) {
-    t.same(specificity.c(selector), [ 0, 2 ])
+test('specificity.vs()', function (t) {
+    t.same(specificity.vs('ul.class', 'ul.class'), 'draw')
     t.end()
 })
